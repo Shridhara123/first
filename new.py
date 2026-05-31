69c8bb42e9112c61758fd9a03577314290356b27
Shridhara123 <shriharsharao11@gmail.com>
2026-05-31 19:53:25 +0530

    c

--- new.py
+++ new.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Manigandan namsakar"
+        "message": "nale office barthira "
     })
 
 # NEW FEATURE
